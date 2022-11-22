ab3a6cc24504de6275e119619ffc874076d32f21
Mako <starykovskyeric@gmail.com>
2022-11-22 14:02:27 -0600

    javascript creates shapes (only squares for now)

--- assets/script/index.js
+++ assets/script/index.js
@@ -14,3 +14,51 @@ function sleep(duration) {
         setTimeout(resolve, duration);
     })
 }
+
+// Selectors -----------------------
+
+const shape = select('.shapeSel');
+const color = select('.colorSel');
+const create = select('.create');
+
+// Class constructor ---------------
+
+class Shape {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    setShape() {
+        if (name === "square") {
+            
+        }
+    }
+
+    getInfo() {
+        return [this.name, this.color];
+    }
+
+}
+
+onEvent('click', create, function() {
+    createShape();
+});
+
+function createShape() {
+    let element = document.createElement('div');
+    document.querySelector('.shapes').appendChild(element);
+    // Confirmation log message
+    console.log('Created element');
+
+    let shapeType = shape.value;
+    let colorType = color.value;
+
+    const newShape = new Shape(shapeType, colorType)
+    // Element size
+    element.style.width = "80px";
+    element.style.height = "80px";
+    element.style.borderRadius = "5px";
+
+    element.style.backgroundColor = "white";
+}
